ab82cf4d96e2ddda80ba813be2643968061a9ace
Kim Jiwon <kjw100404@gmail.com>
2025-01-14 23:53:52 +0900

    [Feat]: 3일차 공부 기록

---
+++ 09Test1.swiftpm/ContentView.swift
@@ -0,0 +1,37 @@
+import SwiftUI
+
+struct ContentView: View {
+    // @State: 기본 상태를 나타냄
+    @State var menu: String? = "Newjeans"
+    @State var buttonText: String = "Hi, Bunnies!"
+    var menus: [Menu] = [.Minji, .Hanni, .Daniel, .Haerin, .Hyein]
+    
+    enum Menu: String {
+        case Minji = "🐶"
+        case Hanni = "🦦"
+        case Daniel = "🦌"
+        case Haerin = "🐱"
+        case Hyein = "🐹"
+    }
+        
+    
+    var body: some View {
+        
+        VStack {
+            if(menu == "Minji" || menu == "Hanni"){
+                Text("2004")
+            } else{
+                Text(menu!)
+            }
+            
+            
+            Button {
+                menu = menus.randomElement()?.rawValue
+                buttonText = "Push This Button!"
+            } label: {
+                Text(buttonText)
+            }
+            
+        }
+    }
+}
